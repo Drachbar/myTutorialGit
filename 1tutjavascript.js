ae26004e95c094c0cbb59adfcb2df691bb128fd7
Drachbar <drachbar@gmail.com>
2020-02-23 21:58:09 +0100

    Second commit of my tutorial js file

--- 1tutjavascript.js
+++ 1tutjavascript.js
@@ -1204,18 +1204,34 @@
     }
     /*20.5 Mutate an Array Declared with const */
     {
-      const s = [5, 7, 2];
-
-      let editInPlace = () => {
-        "use strict"; //denna ger felmeddelande
-        /*s = [2, 5, 7]; */
-
-        s[0] = 2;
-        s[1] = 5; //detta går
-        s[2] = 7;
-      };
-      editInPlace();
-      console.log(s);
+      // const s = [5, 7, 2];
+      // let editInPlace = () => {
+      //   "use strict"; //denna ger felmeddelande
+      //   /*s = [2, 5, 7]; */
+      //   s[0] = 2;
+      //   s[1] = 5; //detta går
+      //   s[2] = 7;
+      // };
+      // editInPlace();
+      // console.log(s);
+    }
+    /*20.6 Prevent object mutation */
+    {
+      // function freezeObj() {
+      //   "use strict";
+      //   const MATH_CONSTANTS = {
+      //     PI: 3.14
+      //   };
+      //   Object.freeze(MATH_CONSTANTS);
+      //   try {
+      //     MATH_CONSTANTS.PI = 99;
+      //   } catch (ex) {
+      //     console.log(ex);
+      //   }
+      //   return MATH_CONSTANTS.PI;
+      // }
+      // const PI = freezeObj();
+      // console.log(PI);
     }
   }
 }
