866ae2f8e6ec4245d01c34e5400a8396e7d4e090
Drachbar <drachbar@gmail.com>
2020-02-24 17:21:29 +0100

    Using git with powershell

--- 1tutjavascript.js
+++ 1tutjavascript.js
@@ -1260,6 +1260,20 @@
       // }
       // console.log(myContact([1, 2], [3, 4, 5]));
     }
+    /*21.3 Write Higher Order Arrow Functions */
+    {
+      const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+
+      const squareList = arr => {
+        const squaredIntegers = arr
+          .filter(num => Number.isInteger(num) && num > 0)
+          .map(x => x * x);
+        return squaredIntegers;
+      };
+
+      const squaredIntegers = squareList(realNumberArray);
+      console.log(squaredIntegers);
+    }
   }
 }
 
