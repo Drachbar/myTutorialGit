baf5763b22f43dd596dee81df3eb332792e7d15c
Drachbar <drachbar@gmail.com>
2020-02-26 22:06:59 +0100

    Added one section

--- 1tutjavascript.js
+++ 1tutjavascript.js
@@ -1260,19 +1260,27 @@
       // }
       // console.log(myContact([1, 2], [3, 4, 5]));
     }
-    /*21.3 Write Higher Order Arrow Functions */
+    /*21.3 Write Higher Order Arrow Functions 1*/
     {
-      const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
-
-      const squareList = arr => {
-        const squaredIntegers = arr
-          .filter(num => Number.isInteger(num) && num > 0)
-          .map(x => x * x);
-        return squaredIntegers;
-      };
-
-      const squaredIntegers = squareList(realNumberArray);
-      console.log(squaredIntegers);
+      // const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+      // const squareList = arr => {
+      //   const squaredIntegers = arr
+      //     .filter(num => Number.isInteger(num) && num > 0)
+      //     .map(x => x * x);
+      //   return squaredIntegers;
+      // };
+      // const squaredIntegers = squareList(realNumberArray);
+      // console.log(squaredIntegers);
+    }
+    /*21.4 Write Higher Order Arrow Functions 2*/
+    {
+      const increment = (function() {
+        return function increment(number, value = 1) {
+          return number + value;
+        };
+      })();
+      console.log(increment(5, 3));
+      console.log(increment(2));
     }
   }
 }
